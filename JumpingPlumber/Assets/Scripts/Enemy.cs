c91eae5a1abca3acd4450ac111f1fbedc92cad86
Alexis-Blouin <116613522+Alexis-Blouin@users.noreply.github.com>
2025-11-01 23:49:59 +0100

    Addition of player health and collisions with enemy

--- JumpingPlumber/Assets/Scripts/Enemy.cs
+++ JumpingPlumber/Assets/Scripts/Enemy.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float speed = 5.0f;
+    [SerializeField] private int damage = 1;
     [SerializeField] private LayerMask groundLayer;
     
     private Rigidbody2D _rb;
@@ -37,8 +38,6 @@ public class Enemy : MonoBehaviour
                 distance,
                 groundLayer);
             
-            DrawBoxCast(_boxCollider.bounds.center, _boxCollider.bounds.size * castSize, angle, Vector2.right, distance, hitRight);
-
             if (hitRight)
             {
                 _direction = -1;
@@ -54,8 +53,6 @@ public class Enemy : MonoBehaviour
                 distance,
                 groundLayer);
             
-            DrawBoxCast(_boxCollider.bounds.center, _boxCollider.bounds.size * castSize, angle, Vector2.left, distance, hitLeft);
-
             if (hitLeft)
             {
                 _direction = 1;
@@ -70,45 +67,10 @@ public class Enemy : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        //Debug.Log("J'ai hit un mur, ayoye!");
-        // Change direction when hitting something
-        //_direction *= -1;
-    }
-    
-    private void DrawBoxCast(Vector2 origin, Vector2 size, float angle, Vector2 direction, float distance, RaycastHit2D hit)
-    {
-        Color color = hit ? Color.green : Color.red;
-
-        // The end position of the cast
-        Vector2 castEnd = origin + direction * distance;
-
-        // Draw the cast line
-        Debug.DrawLine(origin, castEnd, color);
-
-        // Draw the starting box (blue)
-        DrawBox(origin, size, angle, Color.cyan);
-
-        // Draw the end box (green/red)
-        DrawBox(castEnd, size, angle, color);
-    }
-
-    private void DrawBox(Vector2 center, Vector2 size, float angle, Color color)
-    {
-        // Convert angle to radians
-        float rad = angle * Mathf.Deg2Rad;
-        Vector2 right = new Vector2(Mathf.Cos(rad), Mathf.Sin(rad)) * size.x / 2;
-        Vector2 up = new Vector2(-Mathf.Sin(rad), Mathf.Cos(rad)) * size.y / 2;
-
-        // Box corners
-        Vector2 topLeft = center + up - right;
-        Vector2 topRight = center + up + right;
-        Vector2 bottomLeft = center - up - right;
-        Vector2 bottomRight = center - up + right;
-
-        // Draw edges
-        Debug.DrawLine(topLeft, topRight, color);
-        Debug.DrawLine(topRight, bottomRight, color);
-        Debug.DrawLine(bottomRight, bottomLeft, color);
-        Debug.DrawLine(bottomLeft, topLeft, color);
+        if (other.gameObject.CompareTag("Player"))
+        {
+            PlayerHealth playerHealth = other.gameObject.GetComponent<PlayerHealth>();
+            playerHealth.TakeDamage(damage);
+        }
     }
 }
