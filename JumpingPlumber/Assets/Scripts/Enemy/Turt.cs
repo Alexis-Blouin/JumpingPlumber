992ba4e14e35a5d33b2a80e9c0e76fbcd2b6f8bf
Alexis-Blouin <116613522+Alexis-Blouin@users.noreply.github.com>
2025-11-12 22:39:38 +0100

    Turt shell start moving again if me jump on it

--- JumpingPlumber/Assets/Scripts/Enemy/Turt.cs
+++ JumpingPlumber/Assets/Scripts/Enemy/Turt.cs
@@ -10,11 +10,16 @@ public class Turt : Enemy
     
     public override void JumpedOn()
     {
-        _animator.SetTrigger("Die");
-        _isMoving = false;
-        // Adjusts the collider size to fit the sprite
-        _boxCollider.size = new Vector2(_boxCollider.size.x, _deathBoxColliderHeight);
-        // Push the Shroom to the ground after making it slimer 
-        _rb.AddForce(Vector2.down * _deathDownForce);
+        // If turt is not dead yet, we kill it
+        if (!_animator.GetBool("Die"))
+        {
+            _animator.SetTrigger("Die");
+            // Adjusts the collider size to fit the sprite
+            _boxCollider.size = new Vector2(_boxCollider.size.x, _deathBoxColliderHeight);
+            // Push the Shroom to the ground after making it slimer 
+            _rb.AddForce(Vector2.down * _deathDownForce);
+        }
+        // Each time we jump on it, it starts of stops moving
+        _isMoving = !_isMoving;
     }
 }
